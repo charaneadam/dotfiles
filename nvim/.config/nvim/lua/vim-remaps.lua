9a0eef58a895fc3269326d09d51a6df14728b3cb
Adam @ asus <adamcharane@gmail.com>
2024-01-11 11:48:56 +0100

    Make ToggleTerm use indepenedent instances of terminals

--- nvim/.config/nvim/lua/vim-remaps.lua
+++ nvim/.config/nvim/lua/vim-remaps.lua
@@ -16,12 +16,12 @@ vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
 
 -- Terminal
-vim.keymap.set("n", "<M-/>", ":ToggleTerm direction=float<CR>")
-vim.keymap.set("t", "<M-/>", "<C-\\><C-n>:ToggleTerm direction=float<CR>")
-vim.keymap.set("n", "<M-.>", ":ToggleTerm direction=horizontal<CR>")
-vim.keymap.set("t", "<M-.>", "<C-\\><C-n>:ToggleTerm direction=horizontal<CR>")
-vim.keymap.set("n", "<M-,>", ":ToggleTerm direction=vertical<CR>")
-vim.keymap.set("t", "<M-,>", "<C-\\><C-n>:ToggleTerm direction=vertical<CR>")
+vim.keymap.set("n", "<M-/>", ":ToggleTerm 1 direction=float<CR>")
+vim.keymap.set("t", "<M-/>", "<C-\\><C-n>:ToggleTerm 1 direction=float<CR>")
+vim.keymap.set("n", "<M-.>", ":ToggleTerm 2 direction=horizontal<CR>")
+vim.keymap.set("t", "<M-.>", "<C-\\><C-n>:ToggleTerm 2 direction=horizontal<CR>")
+-- vim.keymap.set("n", "<M-,>", ":ToggleTerm direction=vertical<CR>")
+-- vim.keymap.set("t", "<M-,>", "<C-\\><C-n>:ToggleTerm direction=vertical<CR>")
 
 vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>")
 
