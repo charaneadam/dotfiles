a442b1827fc8fcaefc566ee09df51365bcf242ca
Adam <adamcharane@gmail.com>
2024-01-13 13:53:05 +0100

    nvim lsp cmp capabilities

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -24,12 +24,13 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({})
-			lspconfig.texlab.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.clangd.setup({})
+			lspconfig.lua_ls.setup({capabilities = capabilities})
+			lspconfig.texlab.setup({capabilities = capabilities})
+			lspconfig.pyright.setup({capabilities = capabilities})
+			lspconfig.clangd.setup({capabilities = capabilities})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
