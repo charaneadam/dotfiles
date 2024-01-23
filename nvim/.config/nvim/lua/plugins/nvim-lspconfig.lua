f3934eeaec48ab90e5c7049481c4012487937252
Adam <adamcharane@gmail.com>
2024-01-23 02:01:36 +0100

    Update linters and formatters

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -19,10 +19,9 @@ return {
 			require("mason-null-ls").setup({
 				ensure_installed = {
 					"stylua",
-					"pyflakes",
-					"yapf",
 					"flake8",
-					"pylint",
+          "pylint",
+          "black",
 
 					"lua_ls",
 					"texlab",
@@ -35,8 +34,15 @@ return {
 			local null_ls = require("null-ls")
 			null_ls.setup({
 				sources = {
+
+          null_ls.builtins.formatting.black,
+          null_ls.builtins.diagnostics.flake8,
+           null_ls.builtins.diagnostics.pylint,
+
+          null_ls.builtins.formatting.bibclean,
+          null_ls.builtins.formatting.latexindent,
+
 					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.yapf,
 				},
 			})
 		end,
