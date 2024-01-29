f079ffbad013c954569ea0ec7bde596ddb69c7b5
acharane <adamcharane@gmail.com>
2024-01-29 17:04:55 +0100

    Format and remove leader key from lsp capabilities

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -20,8 +20,8 @@ return {
 				ensure_installed = {
 					"stylua",
 					"flake8",
-          "pylint",
-          "black",
+					"pylint",
+					"black",
 
 					"lua_ls",
 					"texlab",
@@ -35,12 +35,12 @@ return {
 			null_ls.setup({
 				sources = {
 
-          null_ls.builtins.formatting.black,
-          null_ls.builtins.diagnostics.flake8,
-           null_ls.builtins.diagnostics.pylint,
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.diagnostics.flake8,
+					null_ls.builtins.diagnostics.pylint,
 
-          null_ls.builtins.formatting.bibclean,
-          null_ls.builtins.formatting.latexindent,
+					null_ls.builtins.formatting.bibclean,
+					null_ls.builtins.formatting.latexindent,
 
 					null_ls.builtins.formatting.stylua,
 				},
@@ -52,48 +52,43 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local keymaps = function()
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-					vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
-					vim.keymap.set("n", "<leader>gn", vim.diagnostic.goto_next, { buffer = 0 })
-					vim.keymap.set("n", "<leader>gp", vim.diagnostic.goto_prev, { buffer = 0 })
-					vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, { buffer = 0 })
-					vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, { buffer = 0 })
-					vim.keymap.set("n", "<leader>fd", ":Telescope diagnostics<CR>", { buffer = 0 })
-				end
+			local keymaps = function()
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+				vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
+				vim.keymap.set("n", "gn", vim.diagnostic.goto_next, { buffer = 0 })
+				vim.keymap.set("n", "gp", vim.diagnostic.goto_prev, { buffer = 0 })
+				vim.keymap.set("n", "gr", vim.lsp.buf.rename, { buffer = 0 })
+				vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { buffer = 0 })
+				vim.keymap.set("n", "<leader>fd", ":Telescope diagnostics<CR>", { buffer = 0 })
+			end
 			lspconfig = require("lspconfig")
 
 			lspconfig.texlab.setup({
 				capabilities = capabilities,
-				on_attach = keymaps
+				on_attach = keymaps,
 			})
-
 
 			lspconfig.clangd.setup({
 				capabilities = capabilities,
-				on_attach = keymaps
+				on_attach = keymaps,
 			})
-
 
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
-				on_attach = keymaps
+				on_attach = keymaps,
 			})
-
 
 			lspconfig.pyright.setup({
 				capabilities = capabilities,
-				on_attach = keymaps
+				on_attach = keymaps,
 			})
-
 
 			lspconfig.rust_analyzer.setup({
 				capabilities = capabilities,
-				on_attach = keymaps
+				on_attach = keymaps,
 			})
-
 		end,
 	},
 }
