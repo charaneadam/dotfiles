35af4cc0ca5e7f798f2df4579798c222e2a48f1a
Adam <adamcharane@gmail.com>
2024-02-26 16:28:09 +0100

    Some snips for latex

---
+++ nvim/.config/nvim/LuaSnip/tex.lua
@@ -0,0 +1,45 @@
+return {
+	s(
+		{ trig = ";env", snippetType = "autosnippet" },
+		fmta(
+			[[
+      \begin{<>}
+          <>
+      \end{<>}
+    ]],
+			{
+				i(1),
+				i(2),
+				rep(1), -- this node repeats insert node i(1)
+			}
+		)
+	),
+
+	s(
+		{ trig = ";item", snippetType = "autosnippet" },
+		fmta(
+			[[
+      \begin{itemize}
+          item <>
+      \end{itemize}
+    ]],
+			{
+				i(0),
+			}
+		)
+	),
+
+	s(
+		{ trig = ";enum", snippetType = "autosnippet" },
+		fmta(
+			[[
+      \begin{enumerate}
+          item <>
+      \end{enumerate}
+    ]],
+			{
+				i(0),
+			}
+		)
+	),
+}
